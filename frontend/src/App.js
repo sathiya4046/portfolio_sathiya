4114536f38ce17973ea72be33618e13b645c11e0
Sathiyaseelan <sathiya.4046@gmail.com>
2024-07-25 17:14:29 +0530

    fourth commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -159,7 +159,7 @@ function App() {
                 <i><small>Using : Reactjs,Bootstrap,Nodejs,Mongodb</small></i>
                 <hr />
                 <p className="card-text">I developed my portfolio website using React.js and Bootstrap combining the innovative of React's component-based architecture with Bootstrap's responsive design capabilities...</p>
-                <a href="#home" className="btn btn-outline-primary">View code</a>
+                <a href="https://github.com/sathiya4046/sathiya-portfolio" className="btn btn-outline-primary">View code</a>
               </div>
             </div>
             <div className="card m-3 p-2">
@@ -186,7 +186,7 @@ function App() {
                 <i><small>Using : Reactjs,Nodejs,Expressjs,Mysql</small></i>
                 <hr />
                 <p className="card-text">I created login and register page with Reactjs for frontend, in backend I use jwt for cookies, bcrypt for password hashing and database is Mysql, it looks like a simple project, I want to implement this idea in future projects.</p>
-                <a href="https://github.com/sathiya4046/crud_keeper_mern" className="btn btn-outline-primary">View code</a>
+                <a href="https://github.com/sathiya4046/auth_project" className="btn btn-outline-primary">View code</a>
               </div>
             </div>
         </div>
